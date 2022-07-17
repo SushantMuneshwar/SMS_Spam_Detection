5d1431bd2a2af4c23f6fb205876353699d8ffc75
SushantMuneshwar <97451321+SushantMuneshwar@users.noreply.github.com>
2022-07-17 23:33:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if st.button ('Predict'):
     transformed_sms = transform_text(input_sms)
 
     # 2. Vectorization
-    vector_input = tfidf.transform(['transformed_sms'])
+    vector_input = tfidf.transform([transformed_sms])
 
     # 3. prediction
     result = model.predict (vector_input) [0]
